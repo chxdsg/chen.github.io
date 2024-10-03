8d2f56eb88c62dbdd601f7bb8a78391a40e75eec
chxdsg <1542672307@qq.com>
2024-10-03 21:48:03 +0800

    index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ function login() {
     var password = document.getElementById('password').value;  
   
     // 这里只是模拟验证过程，实际开发中应该与服务器进行交互  
-    if (username === "chen" && password === "123456") {  
+    if (username === "hanzhuoru" && password === "521910") {  
         // 登录成功，跳转到另一个页面  
         window.location.href = "photo.html";  
     } else {  
